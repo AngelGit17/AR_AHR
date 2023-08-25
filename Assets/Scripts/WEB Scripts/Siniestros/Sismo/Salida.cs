3528aa305790e62c6501e607403ae70ae98aef40
Angel Cruz <angelfortinoc@gmail.com>
2023-08-24 21:41:08 -0600

    ARExport

---
+++ Assets/Scripts/WEB Scripts/Siniestros/Sismo/Salida.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Salida : MonoBehaviour
+{
+    //public GameObject sign;
+    public SismoTimers Time;
+    public GameObject ImaBot;
+    public GameObject ImaVol;
+    public GameObject ImaMoc;
+    public GameObject NotSal;
+    public GameObject flechaslight;
+    public GameObject NotifText;
+    public Text timerfinal;
+    private float timetext;
+
+
+    //public Text text1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time = FindObjectOfType<SismoTimers>();
+    }
+    void Update()
+    {
+        if(ImaBot.activeSelf && ImaMoc.activeSelf && ImaVol.activeSelf){
+            flechaslight.SetActive(true);
+            NotSal.SetActive(true);
+            NotifText.SetActive(true);
+        }
+
+        timerfinal.text = Time.Tiempo.ToString() +" seg.";
+    }
+
+    // Update is called once per frame
+}
