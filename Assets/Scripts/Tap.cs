79fce488bd6ea6412a63912ead1658882c237c9f
Angel Cruz <angelfortinoc@gmail.com>
2023-08-27 22:09:07 -0600

    ExtintorAnim

--- Assets/Scripts/Tap.cs
+++ Assets/Scripts/Tap.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Tap : MonoBehaviour
 {
-    public GameObject imag1;
+    public GameObject extintor;
+    public GameObject extinv;
+    public GameObject extintoranim;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,24 +16,11 @@ public class Tap : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
-    {
-        Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-        RaycastHit raycastHit;
-        if (Physics.Raycast(raycast, out raycastHit))
-        {
-            Debug.Log("Something Hit");
-
-            if (raycastHit.collider.CompareTag("Extintor"))
-            {
-                Debug.Log("Extintor seleccionado");
-            }
-        }
+    public void ButtonExt(){
+        extintor.SetActive(false);
+        extinv.SetActive(true);
     }
-    }
-    void ButtonExt(){
-        Debug.Log("Extintor seleccionado");
+    public void ButtonExtOn(){
+        extintoranim.SetActive(true);
     }
 }
